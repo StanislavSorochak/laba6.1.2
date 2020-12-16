20bf3006086c19092dbc0948ee1c7e8ef052d962
StanislavSorochak <75738611+StanislavSorochak@users.noreply.github.com>
2020-12-17 00:59:19 +0200

    first master commit

---
+++ Test6.1.2/Test6.1.2.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../laba6.1.2/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Test612
+{
+	TEST_CLASS(Test612)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int c;
+			int a[20]{ 9,- 20, - 20,  21,   7, - 12,   8, - 18, - 8,  32,  37,  18, - 14, - 12,  28,  32, - 14,   1, - 10, - 2 };
+			c = Sum(a, 20, 1);
+			Assert::AreEqual(c, 126);
+		}
+	};
+}
---
+++ laba6.1.2/Source.cpp
@@ -0,0 +1,60 @@
+// Lab_06_1_2.cpp
+// < Сорочак Станіслав >
+// Лабораторна робота №6.1.2
+// Пошук елементів одновимірного масиву рекурсивним способом.
+// Варіант 24
+
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+
+using namespace std;
+
+void Create(int* a, const int size, const int Low, const int High, int i)
+{
+    a[i] = Low + rand() % (High - Low + 1);
+    if (i < size - 1)
+        Create(a, size, Low, High, i + 1);
+}
+void Print(int* a, const int size, int i)
+{
+    cout << setw(4) << a[i];
+    if (i < size - 1)
+        Print(a, size, i + 1);
+    else
+        cout << endl;
+}
+int Sum(int* a, const int size, int i)
+{
+    if (i < size)
+    {
+        if (a[i] > 0 && ((i + 1) % 5 != 0))
+            return a[i] + Sum(a, size, i + 1);
+        else
+            return Sum(a, size, i + 1);
+    }
+    else
+        return 0;
+}
+void Null(int* a, const int size, int i)
+{
+    if (a[i] > 0 && ((i + 1) % 5 != 0))
+        a[i] = 0;
+    if (i < size - 1)
+        Null(a, size, i + 1);
+}
+int main()
+{
+    srand((unsigned)time(NULL));
+    const int n = 20;
+    int a[n];
+    int Low = -20;
+    int High = 37;
+
+    Create(a, n, Low, High, 0);
+    Print(a, n, 0);
+    cout << "sum" << Sum(a, n, 0) << endl;
+    Null(a, n, 0);
+    Print(a, n, 0);
+    return 0;
+}
